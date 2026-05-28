4878b5aced9b240a875a53497de03bb6b39c9c3a
BenRoe <BenRoe@users.noreply.github.com>
2026-05-28 17:39:26 +0200

    feat(09.1-02): add VersionedSchema SchemaV1 + AppMigrationPlan to modelContainer
    - Add SchemaV1: VersionedSchema with versionIdentifier 1.0.0
    - Add AppMigrationPlan: SchemaMigrationPlan with stages: [] (no prior version)
    - Wire migrationPlan: AppMigrationPlan.self into .modelContainer call

--- GPSMetadataEditor/GPSMetadataEditorApp.swift
+++ GPSMetadataEditor/GPSMetadataEditorApp.swift
@@ -1,12 +1,23 @@
 import SwiftData
 import SwiftUI
 
+enum SchemaV1: VersionedSchema {
+    static var versionIdentifier: Schema.Version { Schema.Version(1, 0, 0) }
+    static var models: [any PersistentModel.Type] { [RecentCoordinate.self, BatchRunSummary.self] }
+}
+
+enum AppMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [any VersionedSchema.Type] { [SchemaV1.self] }
+    static var stages: [MigrationStage] { [] }
+}
+
 @main
 struct GPSMetadataEditorApp: App {
     var body: some Scene {
         WindowGroup {
             FileIntakeView()
         }
-        .modelContainer(for: [RecentCoordinate.self, BatchRunSummary.self])
+        .modelContainer(for: [RecentCoordinate.self, BatchRunSummary.self],
+                        migrationPlan: AppMigrationPlan.self)
     }
 }
